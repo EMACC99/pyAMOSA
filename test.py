e48d21ecd28248a108a262f29c881797652a5f38
emacc <lalitoceja@gmail.com>
2022-10-03 15:43:27 -0500

    test initialization

--- test.py
+++ test.py
@@ -2,28 +2,38 @@ import numpy as np
 
 from AMOSA import *
 
+
 class Test(AMOSA.Problem):
     def __init__(self):
-        AMOSA.Problem.__init__(self, 2, [AMOSA.Type.INTEGER] * 2, [0] * 2, [100] * 2, 2, 0)
+        AMOSA.Problem.__init__(
+            self,
+            num_of_variables=2,
+            types=[AMOSA.Type.INTEGER] * 2,
+            lower_bounds=[0] * 2,
+            upper_bounds=[100] * 2,
+            num_of_objectives=2,
+            num_of_constraints=0,
+        )
 
     def evaluate(self, x, out):
         out["f"] = x
         pass
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     problem = Test()
-    optimizer = AMOSA()
-    optimizer.archive_hard_limit = 5
-    optimizer.archive_soft_limit = 10
-    optimizer.initial_refinement_iterations = 2
-    optimizer.archive_gamma = 2
-    optimizer.refinement_iterations = 2
-    optimizer.initial_temperature = 50
-    optimizer.final_temperature = 0.0001
-    optimizer.cooling_factor = 0.9
-    optimizer.early_termination = 15
+    config = AMOSAConfig
+    config.archive_hard_limit = 5
+    config.archive_soft_limit = 10
+    config.archive_gamma = 2
+    config.hill_climbing_iterations = 2
+    config.initial_temperature = 50
+    config.final_temperature = 0.0001
+    config.cooling_factor = 0.9
+    config.annealing_iterations = 2500
+    config.early_terminator_window = 15
+
+    optimizer = AMOSA(config)
     optimizer.minimize(problem)
     optimizer.save_results(problem, "test.csv")
     optimizer.plot_pareto(problem, "test.pdf")
-
